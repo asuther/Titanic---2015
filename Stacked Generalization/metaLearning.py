75ec80478fd18eebf51f698ef9e187b2075d5ecd
asuther <alexander.m.sutherland@gmail.com>
2015-12-14 14:30:45 -0800

    Added a package to do custom meta learning
    A package is being created to do meta learning on the titanic data

---
+++ Stacked Generalization/metaLearning.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # Meta-Learning with predictor
+
+class metaLearning():
+    
+    current_model_list = []
+    current_model_param_list = []
+    
+    def __init__():
+        import pandas as pd
+        import numpy as np
+
+    def addModel(model_clf, param_grid):
+        
+        
+        
+        return current_model_list
